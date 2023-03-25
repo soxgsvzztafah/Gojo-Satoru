65723e57ce6f0eae9493a54f133091941f3f23c8
soxgsvzztafah <113221719+soxgsvzztafah@users.noreply.github.com>
2023-03-25 20:01:57 +0530

    ෆක්

--- settings.js
+++ settings.js
@@ -1,69 +1,166 @@
-//═══════════════════════════════════════════════════════//
-//
-//                              𝙶𝙾𝙹𝙾-𝚂𝙰𝚃𝙾𝚁𝚄 𝓫𝔂 𝓷𝓮𝔁𝓾𝓼𝓝𝔀
-//𝙰𝙳𝙾𝙿𝚃𝙴𝙳 𝙵𝚁𝙾𝙼  𝚂𝙲𝚁𝙸𝙿𝚃 𝙾𝙵 𝙲𝙷𝙴𝙴𝙼𝚂𝙱𝙾𝚃 𝚅2 𝙱𝚈 𝙳𝙶𝚇𝚎𝚘𝚗 
-//
-//════════════════════════════//
+/*
+ █▀▀█ █░▒█ █▀▀▀ █▀▀▀ █▄░▒█       █▀▀▀ █░░░ ▀█▀ █▀▀▀█ █▀▀█
+ █░▒█ █░▒█ █▀▀▀ █▀▀▀ █▒█▒█       █▀▀▀ █░░░ ░█░ ▀▀▀▄▄ █▄▄█
+ ▀▀█▄ ▀▄▄▀ █▄▄▄ █▄▄▄ █░░▀█       █▄▄▄ █▄▄█ ▄█▄ █▄▄▄█ █░▒█
+
+WHATSAPP BOT BY MR NIMA ( DARK MAKER OFC )
+
+WHATSAPP - 94719574492
+WEB SITE - http://queenelisa.42web.io/
+SUPPORT GROUP - https://t.me/queenelisasupport
+YOUTUBE - http://youtube.com/MRNIMAOFC
+
+
+*/
 
 const fs = require('fs')
 const chalk = require('chalk')
 
-//Api Website\\
-global.APIs = {
-	zenz: 'https://zenzapis.xyz',
-}
 
-//Api Key\\
-global.APIKeys = {
-	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
-}
+/*
+⚙️ BOT SETTINGS  ⚙️
+*/
+//👇 if you need seen massage use " true "
+global.READ_MASSAGE = false
+//👇 enable desable Girls Voice Reply
+global.VOICE_REPLY = false
+//👇 Do you want show time on your bio use "true" want desable use "false"
+global.AUTO_BIO = false
+//👇 Inbox massage block PM block
+global.INBOX_BLOCK = false
+//👇 Auto react 
+global.AUTO_REACT = false
+//👇 Bad word Auto delete ( you must add bad words )
+global.ANTI_BADWORD = false
+//👇 Kick And Auto Delete Group link Senders
+global.ANTI_G_LINK = false
+//👇 212 Number block
+global.NUMBER_212_BLOCK = false
+//👇 Send Welcome ( true or false )
+global.SEND_WELCOME = process.env.SEND_WELCOME || false
+//👇 Send block command message (true or false )
+global.BLOCK_CMD_MESSAGE_SEND = false
+//👇 Send Auto reply , sticker , voice Use true and costemize your own 
+global.AUTO_FUNCTION = false
+//👇 Put Bot Offline
+global.BOT_OFFLINE = false
+//👇 Only PreFix [ true / false ]
+global.ONLY_PREFIX = false
+//👇 This uplaod youtube videos without limit ⚠️ if you use this you must have paid sever
+global.UPLOAD_MAX = false
+/*
+       ✨ BOT INFO SETTINGS ✨
+*/
+// You Bots Owner Number 
+global.owner = ['94762817504'] 
+//👇 Your Caption ( Image Video )
+global.cap = '© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙳𝙴𝚅𝙸𝙻 𝙱𝙾𝚃 ²⁰²³'
+//👇 Your Bot Name
+global.botnma =  '𝙳𝙰𝚁𝙺 𝙳𝙴𝚅𝙸𝙻 𝙱𝙾𝚃' 
+//👇 Your name
+global.ownernma =  '𝙳𝙰𝚁𝙺 𝙳𝙴𝚅𝙸𝙻' 
+//👇 Sticker Author Name
+global.packname =  '𝚃𝙴𝙰𝙼 𝙳𝙰𝚁𝙺 𝙳𝙴𝚅𝙸𝙻' 
+//👇 Inbox Block Massage 
+global.INBOX_BLOCK_MSG = 'BLOCK !!!'
+//👇 Owner React Imoji
+global.OWNER_REACT =  '👨‍💻'
+//👇 Song Download Massage 
+global.SONG_DOWN = '```📥𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳𝙸𝙽𝙶 𝚈𝙾𝚄𝚁 𝚂𝙾𝙽𝙶...```' 
+//👇 Song Uplode massage 
+global.SONG_UP = '```📤𝚄𝙿𝙻𝙾𝙰𝙳𝙸𝙽𝙶 𝚈𝙾𝚄𝚁 𝚂𝙾𝙽𝙶..```'
+//👇 Menu imoji 
+global.MENU_IMOJI = '🤠|🗨️️'
+//👇 Your time zone ( use correct time zone )
+global.TIME_ZONE = process.env.TIME_ZONE || 'Asia/Colombo'
+//👇 Removebg Api key
+global.REMOVE_BG_API = 'apikey get on remoebg.com'
+//👇 Pron Video Download 
+global.SEX_VIDEO_DOWNLOAD = process.env.SEX_VIDEO_DOWNLOAD || 'false'
+//👇 Change Bot Language 
+global.LANG = 'EN'
+//Alive logo
+global.alivelogo = process.env.ALIVE_IMAGE || `https://telegra.ph/file/b3f17c16eedf99731a52d.jpg`
+//👇 Alive message
+global.ALIVE_MESSAGE = process.env.ALIVE_MESSAGE || `default`
+//👇 Welcome Mesaage
+global.WELCOME = process.env.WELCOME || ``
+//👇 welcome image 
+global.WELCOME_IMAGE = process.env.WELCOME_IMAGE || ``
+//👇 Goodbye message 
+global.GOODBYE = process.env.GOODBYE || ``
+//👇 Good bye image
+global.GOODBYE_IMAGE = process.env.GOODBYE_IMAGE || ``
+//👇 Desable PM Block numbers
+global.NO_BLOCK = ["94762817504","94719574492"]
+//👇 Desable download words
+global.IMG_BLOCKER = ["xxx","sex","mia"]
+//👇 Open ai API
+global.CHATGPT_API = ''
+/*
 
-//customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
-global.ig = 'https://github.com/nexusNw'
-global.region = 'India, South India, Kerala'
-global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
-global.sessionName = 'session'
-global.prefa = ['','!','.','🐦','🐤','🗿']
-global.sp = '⭔'
+   🍃 OTHER 🍃
+
+*/
+global.S_CAP = ''
+global.V_CAP = ''
+global.YT_CAP = ''
+global.PORT = '8000'
+global.zenzapi = '01ABEB1E11'
+/*
+
+💬 MESSAGE 💬
+
+*/
 global.mess = {
-    success: 'Done ✓',
-    admin: 'This Feature Is Only For Admin!',
-    botAdmin: 'Bot Must Be Admin First!',
-    owner: 'This Feature Is Only For Owner!',
-    group: 'Feature Used Only For Groups!',
-    private: 'Features Used Only For Private Chat!',
-    bot: 'This Feature Is Only For Bot',
-    wait: 'Wait A minute...',
-    error: 'Error! Maybe Api Key Is Expired🤔!',
+    success: '✅ Done!',
+    admin: '*Only admins can use this command !*',
+    botAdmin: '*Please admin the bot first !*',
+    owner: '*Sorry, this command is only for the owner !*',
+    group: '*This command can only be used in groups !*',
+    private: '*This command can only be used in Inbox !*',
+    bot: '*This command can only be used by bot number !*',
+    wait: '*♲ Please Wait ...*',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+    BLOCK_CMD_MSG : '*⚠️ This Command is Block By Owner*',
+    IMG_BLOCK : "This Word is Ban from image downloader",
+    BAD_DETECT : '',
+    BLOCK : '',
+    UNBLOCK : '',
+    KICK : '',
+    ADD : '',
+    LEAVE : '',
+    PROMOTE : '',
+    DEMOTE : ''
 }
-    global.limitawal = {
-    premium: "Infinity",
-    free: 12,
-    monayawal: 1000
+
+
+
+
+//other
+global.pemilik = ['94762817504'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
+global.premium = ['94762817504'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
+global.pengguna = 'DARK DEVIL' // Your name ඔබේ නම 
+global.sessionName = 'session'
+global.prefix = ['#','!','.'] 
+global.sp = '👹'
+global.weem = 'DEVIL BOT'
+
+global.limitawal = {
+    premium: "Infinity", 
+    free: 90 
 }
-   global.rpg = {
-   darahawal: 100,
-   besiawal: 15,
-   goldawal: 10,
-   emeraldawal: 5,
-   umpanawal: 5,
-   potionawal: 1
+//global api
+global.fbapi = 'dd79-1aeb-21a3'
+global.APIs = {
+	zenz: 'https://zenzapi.xyz', 
 }
-global.thumb = fs.readFileSync('./GojoMedia/gojo.jpg')
-global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.APIKeys = {
+	'https://zenzapi.xyz': '01ABEB1E11', //Kalau habis beli sendiri
+}
+
+//global.alivelogo = `https://telegra.ph/file/b3f17c16eedf99731a52d.jpg`
+global.imgalive = fs.readFileSync('./Media/image/Elisa.jpg')
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
 	fs.unwatchFile(file)
